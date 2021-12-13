f5f31a3a8114a53e60e34712b6bc417e262892b2
Sean Mackesey <s.mackesey@gmail.com>
2021-12-13 09:35:35 -0800

    revert ConfigType isinstance checks

--- python_modules/dagster/dagster/config/iterate_types.py
+++ python_modules/dagster/dagster/config/iterate_types.py
@@ -2,23 +2,26 @@ from typing import Dict, Literal, Union, cast
 from dagster import check
 from dagster.config.field import Field
 
-from .config_type import Array, Noneable, ConfigType, ConfigTypeKind, ScalarUnion
+from .config_type import ConfigType, ConfigTypeKind
 from .snap import ConfigSchemaSnapshot, snap_from_config_type
 
 
 def iterate_config_types(config_type: ConfigType):
     check.inst_param(config_type, "config_type", ConfigType)
-    if isinstance(config_type, (Array, Noneable)):
-        yield from iterate_config_types(config_type.inner_type)
+
+    # type-ignore comments below are because static type checkers don't
+    # understand the `ConfigTypeKind` system.
+    if config_type.kind == ConfigTypeKind.ARRAY or config_type.kind == ConfigTypeKind.NONEABLE:
+        yield from iterate_config_types(config_type.inner_type)  # type: ignore
 
     if ConfigTypeKind.has_fields(config_type.kind):
         fields = cast(Dict[str, Field], config_type.fields)  # type: ignore
         for field in fields.values():
             yield from iterate_config_types(field.config_type)
 
-    if isinstance(config_type, ScalarUnion):
-        yield config_type.scalar_type
-        yield from iterate_config_types(config_type.non_scalar_type)
+    if config_type.kind == ConfigTypeKind.SCALAR_UNION:
+        yield config_type.scalar_type  # type: ignore
+        yield from iterate_config_types(config_type.non_scalar_type)  # type: ignore
 
     yield config_type
 
